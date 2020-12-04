1a0c65c25f6c27ea384fb522d0bc965ed3643ccf
alblushi-cyber <alblushi-cyber@users.noreply.github.com>
2020-12-04 11:10:49 +0400

    Add project files.

---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte num1;
+            num1 = 5;
+            Console.WriteLine(num1);
+
+            String Num2;
+            Num2 = "Hello World ";
+            Console.WriteLine(Num2);
+
+            ulong num3;
+            num3 = 9;
+            Console.WriteLine(num3);
+
+            int intNum;
+            intNum = 144;
+            Console.WriteLine(intNum);
+
+            char Litter;
+            Litter = 'i';
+            Console.WriteLine(Litter);
+
+           //By MAZEN instagram c64d
+
+        }
+    }
+}
